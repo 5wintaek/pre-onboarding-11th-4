d5c0686ef9f57120f978b75fc60f6ba285e91196
Seung taek Oh <elqkddl@naver.com>
2023-07-19 21:23:09 +0900

    [Feat] Debounce 적용 완료

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,14 +1,23 @@
 import { SearchContextType, sickContext } from '@/context/SickContext';
-import { ChangeEvent, useContext, useState } from 'react';
+import { ChangeEvent, useContext, useEffect, useState } from 'react';
 import { RecommendList } from '../RecommendList';
 import * as S from './SearchBar.style';
 import { SearchIcon } from '@/assets/icon';
+import { useDebounce } from '@/hooks/useDebounce';
 
 export function SearchBar() {
-  const { recommendValue, fetchRecommendData, setRecommendValue } = useContext(
+  const { recommendValue, fetchRecommendData } = useContext(
     sickContext
   ) as SearchContextType;
   const [inputValue, setInputValue] = useState<string>('');
+  const debounce = useDebounce(inputValue, 300);
+
+  useEffect(() => {
+    if (debounce) {
+      fetchRecommendData(debounce);
+      console.log(debounce);
+    }
+  }, [debounce]);
 
   const handleClick = async () => {
     // 여기는 이동처리만
@@ -27,17 +36,6 @@ export function SearchBar() {
     const value = e.target.value;
     setInputValue(value);
     // await fetchRecommendData(inputValue);
-
-    if (e.target.value === '') {
-      setRecommendValue([]);
-    } else {
-      try {
-        const response = await fetchRecommendData(value);
-        console.log(response);
-      } catch (error) {
-        console.error();
-      }
-    }
 
     // if (e.target.value === '') {
     //   setRecommendValue([]);
